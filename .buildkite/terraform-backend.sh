767ee5e8d4c52f5d65dff81097b76c4d2fcc2ab0
Camilo Rivera <camilo@riverapineda.com>
2017-12-27 11:40:21 -0600

    testing terraform init

--- .buildkite/terraform-backend.sh
+++ .buildkite/terraform-backend.sh
@@ -2,5 +2,5 @@
 
 set -eo pipefail
 echo "--- Init Terraform Backend"
-ls -a terraform/aws/backend
-terraform init -input=false teraform/aws/backend
+cd terraform/aws/backend
+terraform init -input=false
