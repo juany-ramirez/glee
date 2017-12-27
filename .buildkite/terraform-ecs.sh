d676957ad16abce7d58fd84599eb4ba5ba76e897
Camilo Rivera <camilo@riverapineda.com>
2017-12-27 13:06:34 -0600

    start ecs creation

--- .buildkite/terraform-ecs.sh
+++ .buildkite/terraform-ecs.sh
@@ -5,7 +5,7 @@ echo "--- Init Terraform Backend"
 cd terraform/aws/ecs
 terraform init -input=false
 echo "--- Create Workspace for $ENVIRONMENT"
-../../../.buildkite/terraform-create-workspace.sh
+sudo ../../../.buildkite/terraform-create-workspace.sh
 echo "--- Select Workspace for $ENVIRONMENT"
 terraform workspace select $ENVIRONMENT
 
