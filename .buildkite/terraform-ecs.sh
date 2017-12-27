10cb293f98dc440fc4b47b18e26f6d824d1e332f
Camilo Rivera <camilo@riverapineda.com>
2017-12-27 13:17:32 -0600

    create ecr

--- .buildkite/terraform-ecs.sh
+++ .buildkite/terraform-ecs.sh
@@ -9,5 +9,9 @@ chmod +x ../../../.buildkite/terraform-create-workspace.sh
 ../../../.buildkite/terraform-create-workspace.sh
 echo "--- Select Workspace for $ENVIRONMENT"
 terraform workspace select $ENVIRONMENT
+echo "--- Plan for $ENVIRONMENT"
+terraform plan -out=tfplan -input=false
+echo "--- Apply for $ENVIRONMENT"
+terraform apply -input=false tfplan
 
 
--- terraform/aws/ecs/ecs.tf
+++ terraform/aws/ecs/ecs.tf
@@ -5,3 +5,7 @@ terraform {
     region = "us-east-1"
   }
 }
+
+resource "aws_ecr_repository" "repository" {
+  name = "bar"
+}
