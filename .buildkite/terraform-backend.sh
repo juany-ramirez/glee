12be9cd929b67d383e8a2cbbae709d6a217fec5c
Camilo Rivera <camilo@riverapineda.com>
2017-12-27 12:33:49 -0600

    capture when a workspace is already created

--- .buildkite/terraform-backend.sh
+++ .buildkite/terraform-backend.sh
@@ -9,6 +9,7 @@ set +e
 terraformmessage=$(terraform workspace new $ENVIRONMENT 2>&1 1>/dev/null)
 set -e
 if [ $? -eq 0 ]; then
+   echo $?
    echo "not right now !!! $terraformmessage"
    exit 0
 else
