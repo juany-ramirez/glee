c2ea97e30dbb0ce863031c824ffc2077d85e3fb7
Camilo Rivera <camilo@riverapineda.com>
2017-12-27 12:09:25 -0600

    capture when a workspace is already created

--- .buildkite/terraform-backend.sh
+++ .buildkite/terraform-backend.sh
@@ -5,12 +5,15 @@ echo "--- Init Terraform Backend"
 cd terraform/aws/backend
 terraform init -input=false
 echo "--- Create Workspace for $ENVIRONMENT"
+set +e
 terraformmessage=$(terraform workspace new $ENVIRONMENT 2>/dev/null)
+echo $?
+set -e
 if [ "$terraformmessage" == "Workspace \"$ENVIRONMENT\" already exists" ]; then
    echo "Workspace $ENVIRONMENT already exists"
    exit 0
 else
   echo $terraformmessage
-  exit 1
+  #exit 1
 fi
 
