ad1e2b502f84e7c5c689cc3d88d87a640e970a90
Camilo Rivera <camilo@riverapineda.com>
2017-12-28 12:32:38 -0600

    creating VPC

--- .buildkite/terraform-vpc.sh
+++ .buildkite/terraform-vpc.sh
@@ -3,12 +3,11 @@
 set -eo pipefail
 echo "--- Init Terraform Backend"
 export TF_VAR_APPNAME=$APPNAME
-export ENVIRONMENT="share"
-cd terraform/aws/elasticbeanstalk/application
-terraform init -input=false -backend-config="key=$APPNAME/eb-application"
+cd terraform/aws/vpc
+terraform init -input=false -backend-config="key=$APPNAME/vpc"
 echo "--- Create Workspace for $ENVIRONMENT"
-chmod +x ../../../../.buildkite/terraform-create-workspace.sh
-../../../../.buildkite/terraform-create-workspace.sh
+chmod +x ../../../.buildkite/terraform-create-workspace.sh
+../../../.buildkite/terraform-create-workspace.sh
 echo "--- Select Workspace for $ENVIRONMENT"
 terraform workspace select $ENVIRONMENT
 echo "--- Plan for $ENVIRONMENT"
---
+++ terraform/aws/vpc/vpc.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "acklen-terraform-state"
+    region = "us-east-1"
+  }
+}
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_ecr_repository" "repository" {
+  name = "${var.APPNAME}"
+}
