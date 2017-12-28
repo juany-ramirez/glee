7f68a6414d50bf703937597feaf8161fdd845429
Camilo Rivera <camilo@riverapineda.com>
2017-12-28 13:50:20 -0600

    create natgatway

--- terraform/aws/vpc/vpc.tf
+++ terraform/aws/vpc/vpc.tf
@@ -26,5 +26,32 @@ resource "aws_subnet" "main" {
     Name = "${var.APPNAME}-${terraform.workspace}-${element(var.SUBNETS[element(keys(var.SUBNETS),count.index)],0)}-${element(var.SUBNETS[element(keys(var.SUBNETS),count.index)],1)}"
     Project = "${var.APPNAME}"
     Environment = "${terraform.workspace}"
+    Type = "${element(var.SUBNETS[element(keys(var.SUBNETS),count.index)],1)}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name = "${var.APPNAME}-${terraform.workspace}-gw"
+    Project = "${var.APPNAME}"
+    Environment = "${terraform.workspace}"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id     = "${aws_subnet.main.0.id}"
+  depends_on = ["aws_internet_gateway.gw","aws_eip.nat"]
+
+  tags {
+    Name = "${var.APPNAME}-${terraform.workspace}-nat-gw"
+    Project = "${var.APPNAME}"
+    Environment = "${terraform.workspace}"
   }
 }
