d5e0703f1f0a6def094b2293343dbe13e3d9ef56
Camilo Rivera <camilo@riverapineda.com>
2017-12-28 13:19:57 -0600

    test to create a loop of subnets

--- terraform/aws/vpc/variables.tf
+++ terraform/aws/vpc/variables.tf
@@ -1,1 +1,14 @@
 variable "APPNAME" {}
+variable "SUBNETS" {
+  type    = "map"
+  default = {
+    "10.0.0.0/20" = ["us-east-1a","public"]
+    "10.0.16.0/20" = ["us-east-1a","private"]
+    "10.0.32.0/20" = ["us-east-1b","public"]
+    "10.0.48.0/20" = ["us-east-1b","private"]
+    "10.0.64.0/20" = ["us-east-1c","public"]
+    "10.0.80.0/20" = ["us-east-1c","private"]
+    "10.0.96.0/20" = ["us-east-1d","public"]
+    "10.0.112.0/20" = ["us-east-1d","private"]
+  }
+}
--- terraform/aws/vpc/vpc.tf
+++ terraform/aws/vpc/vpc.tf
@@ -12,5 +12,19 @@ resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   tags {
     Name = "${var.APPNAME}-${terraform.workspace}-vpc"
+    Project = "${var.APPNAME}"
+    Environment = "${terraform.workspace}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = "${aws_vpc.main.id}"
+  count    = "${length(var.SUBNETS)}"
+  cidr_block = "${keys(var.SUBNETS)[count.index]}"
+  availability_zone = "${var.SUBNETS[keys(var.SUBNETS)[count.index]][0]}"
+  tags {
+    Name = "${var.APPNAME}-${terraform.workspace}-subnet-${var.SUBNETS[keys(var.SUBNETS)[count.index]][1]}"
+    Project = "${var.APPNAME}"
+    Environment = "${terraform.workspace}"
   }
 }
